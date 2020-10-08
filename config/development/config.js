dcbc256ec6e48aaa6a80f879b44c4fc65a33e261
arjun-707 <ajunakki707@gmail.com>
2020-10-08 17:16:23 +0530

    readme edited

--- config/development/config.js
+++ config/development/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   googleBook: {
-    apiKey:'AIzaSyCMBuLeRVqeRmnEeERRqxGSvNaz8xjPh-8'
+    apiKey: 'AIzaSyCMBuLeRVqeRmnEeERRqxGSvNaz8xjPh-8',
+    clientId: '628324260473-pg24qe3smkm1n8dtmb2djov8o6afne0d.apps.googleusercontent.com',
+    clientSecret: 'rpAp5Ns1i-q5IBoseDk1bdj1'
   }
 }
